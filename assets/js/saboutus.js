5a61e1959196d2d50d9d32e645e3dfe897db6c9f
NinYuri <yurianamontserrath2003@gmail.com>
2024-05-19 01:09:25 -0600

    About Us in Progress

---
+++ assets/js/saboutus.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/* PRELOAD */
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+// add event listener on multiple elements
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+/* NAVBAR */
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+// dejar elemento seleccionado del nav
+function setActive(element) {
+  var navbarLinks = document.querySelectorAll(".navbar-link");
+
+  navbarLinks.forEach(function(link) {
+      link.classList.remove("active");
+  });
+  element.classList.add("active");
+}
+
+var navbarItems = document.querySelectorAll(".navbar-item");
+
+navbarItems.forEach(function(item) {
+  item.addEventListener("click", function() {
+      setActive(this.querySelector("a"));
+  });
+});
+
+
+/* HERO SLIDER */
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSliderPos = function () {
+  lastActiveSliderItem.classList.remove("active");
+  heroSliderItems[currentSlidePos].classList.add("active");
+  lastActiveSliderItem = heroSliderItems[currentSlidePos];
+}
+
+const slideNext = function () {
+  currentSlidePos = (currentSlidePos + 1) % heroSliderItems.length;
+  updateSliderPos();
+}
+
+// auto slide
+let autoSlideInterval;
+
+const autoSlide = function () {
+  autoSlideInterval = setInterval(function () {
+    slideNext();
+  }, 7000);
+}
+
+window.addEventListener("load", autoSlide);
