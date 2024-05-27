29bee4cc5d1c8936a064f17fb9e5979c24651b18
NinYuri <yurianamontserrath2003@gmail.com>
2024-05-27 00:04:30 -0600

    About Us Complete

--- assets/js/saboutus.js
+++ assets/js/saboutus.js
@@ -48,6 +48,35 @@ navbarItems.forEach(function(item) {
 });
 
 
+/* HEADER & BACK TOP BTN */
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+  const isScrollBottom = lastScrollPos < window.scrollY;
+  if (isScrollBottom) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+
+  lastScrollPos = window.scrollY;
+}
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+    hideHeader();
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
 /* HERO SLIDER */
 const heroSlider = document.querySelector("[data-hero-slider]");
 const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
